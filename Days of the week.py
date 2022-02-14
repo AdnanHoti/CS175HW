515401d1133e95f04039a2a31059374cd3f323dd
AdnanHoti <91210634+AdnanHoti@users.noreply.github.com>
2022-02-14 10:33:47 -0500

    Day Of the week

---
+++ Days of the week.py
@@ -0,0 +1,30 @@
+#Adnan Hoti
+#Days of the Week
+
+day  = int(input("Enter a number in the range of 1 to 7 : ")) 
+
+
+
+if(day == 1):
+    print("Monday")
+
+elif(day == 2):
+    print("Tuesday")
+
+elif(day == 3):
+    print("Wednesday")
+
+elif(day == 4):
+    print("Thursday")
+
+elif(day == 5):
+    print("Friday")
+
+elif(day == 6):
+    print("Saturday")
+
+elif(day == 7):
+    print("Sunday")
+
+else:
+    print("Error!") 
