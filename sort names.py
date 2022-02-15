3ae456e1b5de7e4d4dcf8b8a4ccbb38301144682
AdnanHoti <91210634+AdnanHoti@users.noreply.github.com>
2022-02-14 22:59:06 -0500

    sorted names

---
+++ sort names.py
@@ -0,0 +1,12 @@
+#Adnan Hoti
+#Sorted Names
+
+name1 = input('What is name 1:')
+name2 = input('What is name 2:')
+name3 = input('What is name 3:')
+total_names =(name1,name2,name3)
+
+
+print(sorted(total_names))
+
+
