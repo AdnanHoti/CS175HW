994feec9fdf57adaf37e69d267dbe14a1f021566
AdnanHoti <91210634+AdnanHoti@users.noreply.github.com>
2022-02-25 21:48:01 -0500

    Weather station V3

--- New wind2.py
+++ New wind2.py
@@ -3,7 +3,38 @@ windspeed_valid = True
 temp_fah = 34.6
 temp_cel = (5/9) * (temp_fah-32)
 wind_speed = 23
+
 station_name =input('What is the Weather Station Name?:')
+
+input('Welcome to [user’s input] Weather Station.')
+
+Option1 = input('Option 1: Input')
+Option2 = input('Option 2: Print')
+Option3 = input('Option 3: Exit')
+Choice = input("Enter your choice:")
+
+if Choice == 'Option 2':
+    print('The temp is:', temp_fah)
+    print('The cel is:', temp_cel)
+    print('The wind speed is:', wind_speed)
+
+elif Choice == 'Option 1':
+    temp = int(input('What is the temp:'))
+if temp < -130 or temp > 150 :
+    print('INVALID!!')
+windspeed = int(input('What is the wind speed?:'))
+if windspeed < 0:
+    print('INVALID')
+direction = int(input('What is wind direction?:'))
+
+if Choice == 'Option 3':
+    print('Goodbye!')
+    quit()
+
+if Choice > 3:
+    print('Choose valid choice: 1,2,3')
+    
+
 
 message = input('\n Input weather reading? (y/Y for yes, n/N for no):')
 if message.lower() == 'no':
@@ -16,6 +47,8 @@ if temp_fah < -130 or temp_fah > 150:
     temp_valid = False
 else:
     temp_cel = (5/9) * (temp_fah-32)
+    
+
     
 wind_speed = int(input('What is the wind speed?:'))
 if wind_speed < 0:
