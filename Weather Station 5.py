a5c3d034c52fc38c486de37f539c05310bfaca13
AdnanHoti <91210634+AdnanHoti@users.noreply.github.com>
2022-04-04 18:01:36 -0400

    updated

--- Weather Station 5.py
+++ Weather Station 5.py
@@ -5,36 +5,27 @@ temp_fah = 34.6
 temp_cel = (5/9) * (temp_fah-32)
 wind_speed = 23
 wind_direction = ''
-
-
-
-#Open the file with the "w" tag to enter write mode
-#Write mode deletes all previous file contnets, so every time your program runs it will clear the file and re make the column headers
-file = open("out.txt","w")
-
-#Write the column headers
-file.write("Temperature \t Wind Speed \t Wind Direction")
-
-#Clsoe the file so we can open it in append mode later
-file.close()
-
 user_input =input('What is the Weather Station Name?:')
 print('\t'+user_input+' Weather Station')
 
-file = open("out.txt","a")
-
-#Build function to write to the file
-def write(file_to_write, temp,speed,direction):
-    #This is how you write to a file
-    file_to_write.writelines("\n" + temp + "\t" + speed + "\t\t" + direction)
-    #Close it after so we can read it later
-    file_to_write.close()
+file = open("out.txt","w")
+file.write("Temperature \t Wind Speed \t Wind Direction")
+file.close()
 
 def show_menu():
     print('Please select one of the following choices.')
     print('Option 1: Input')
     print('Option 2: Print')
     print('Option 3: Exit')
+
+
+#Build function to write to the file
+def write(file_to_write, temp,speed,direction):
+    #This is how you write to a file
+    file_to_write.writelines("\n" + temp + "\t\t" + speed + "\t\t" + direction)
+    #Close it after so we can read it later
+    file_to_write.close()
+
 
 def input_weather():
     temp_fah = float(input('What is the current tempature(Fahrenheit)?:'))
@@ -51,22 +42,22 @@ def input_weather():
         windspeed_valid = False
     
     wind_direction = input('What is wind direction?:')
+    file = open("out.txt","a")
+    write(file, str(temp_fah),str(wind_speed),wind_direction)
     return wind_speed, temp_fah, wind_direction
 
 def print_weather(temp_f,wind_speed,wind_direction):
     file = open("out.txt","r")
-    #Read lines
-    lines = file.readlines()
-    for line in lines:
+    for line in file:
         print(line)
     file.close()
+
 
 def valid_choice(choice):
     if choice == '1' or choice == '2' or choice =='3':
         return True
     else:
         return False
-
 
 inputed = False
 while loop == True:
@@ -77,12 +68,8 @@ while loop == True:
         if (option_input == '1'):
             inputed = True
             readings = input_weather()
-            temp_fah=readings[1]
-            wind_speed=readings[0]
-            wind_direction=readings[2]
-            write(file, temp_fah, wind_speed, wind_direction)
         elif (option_input == '2'):
-            if inputed == True:
+            if inputed==True:
                 print_weather(temp_fah, wind_speed, wind_direction)
             else:
                 print("No weather entered. Visit option 1 first.")
@@ -92,4 +79,3 @@ while loop == True:
             loop = False
     elif valid == False:
         print("Invalid!")
-        continue
